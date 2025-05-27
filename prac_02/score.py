4e6101fb28cdb3dee53b6bd8df78fb9f44d809de
29034 <junjie.chu@my.jcu.edu.au>
2025-05-27 15:31:25 +0800

    Add password check program

---
+++ prac_02/score.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 - Practical
+Program to determine score status with constants
+"""
+MAX_SCORE = 100
+MIN_SCORE = 0
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
+message = ""
+
+score = float(input("Enter score: "))
+
+# Check and display result
+if score < MIN_SCORE or score > MAX_SCORE:
+    print("Invalid score")
+else:
+    if score >= EXCELLENT_THRESHOLD:
+        message = "Excellent"
+    elif score >= PASS_THRESHOLD:
+        message = "Passable"
+    else:
+        message = "Bad"
+
+print(message)
