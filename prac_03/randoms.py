7940114f58531efa84464a3a2e689f845b718cc2
29034 <junjie.chu@my.jcu.edu.au>
2025-06-07 14:18:40 +0800

    Add prac 3 files

---
+++ prac_03/randoms.py
@@ -0,0 +1,9 @@
+import random
+
+print(random.randint(5, 20))      # min=5, max=20
+
+print(random.randrange(3, 10, 2))      # min=3, max=9, cannot be 4
+
+print(random.uniform(2.5, 5.5))      # min≈2.5, max≈5.5
+
+print(random.randint(1, 100))
