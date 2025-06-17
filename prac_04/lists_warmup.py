233dbf537f6821ccd595f0232388e092a9c8520f
29034 <junjie.chu@my.jcu.edu.au>
2025-06-17 15:15:32 +0800

    Add list indexing, slicing, and edit tasks

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -13,7 +13,7 @@ print(numbers + [6, 5, 3])  # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 # Change the first element of numbers to "ten" (the string, not the number 10)
 numbers[0] = "ten"
 
-# Change the last element of numbers to 1
+# Change the last element of numbers to 1.
 numbers[-1] = 1
 
 # Print all the elements from numbers except the first two (slice)
