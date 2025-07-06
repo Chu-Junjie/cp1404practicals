726c503e9f4df00c082b64e5bcf7626cfaafb90d
29034 <junjie.chu@my.jcu.edu.au>
2025-07-06 18:02:26 +0800

    Added __str__ methods

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -18,3 +18,6 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Determine if the programming language is dynamically typed."""
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
