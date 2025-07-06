3fc83d20287d110003983c94371631fbe3fa44f2
29034 <junjie.chu@my.jcu.edu.au>
2025-07-06 18:11:53 +0800

    Created Guitar class and init method

---
+++ prac_06/guitar.py
@@ -0,0 +1,12 @@
+"""
+guitar
+Estimate: 20 minutes
+Actual:    minutes
+"""
+class Guitar:
+    """Represent a guitar with name, year, and cost."""
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
