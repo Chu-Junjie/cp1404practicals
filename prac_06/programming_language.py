6f67b3b199c4db2d327b04c78102b58d74f1b423
29034 <junjie.chu@my.jcu.edu.au>
2025-07-11 09:29:13 +0800

    Refactor code reflection from "" to False

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,11 +8,11 @@ Actual:   15 minutes
 class ProgrammingLanguage:
     """Represent a programming language."""
 
-    def __init__(self, name="", typing="", reflection="", year=0):
+    def __init__(self, name="", typing="", is_reflection=False, year=0):
         """Initialise a Programming Language instance."""
         self.name = name
         self.typing = typing
-        self.reflection = reflection
+        self.is_reflection = is_reflection
         self.year = year
 
     def is_dynamic(self):
@@ -21,4 +21,4 @@ class ProgrammingLanguage:
 
     def __str__(self):
         """Return a formatted string with details about the programming language."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.is_reflection}, First appeared in {self.year}"
