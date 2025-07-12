cc2ca383afeea790f6d1e0151872230bb6f82dc1
29034 <junjie.chu@my.jcu.edu.au>
2025-07-12 20:21:49 +0800

    Add load_projects function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -46,5 +46,18 @@ def main():
         else:
             print("Invalid option")
 
+def load_projects(filename):
+    projects = []
+    with open(filename, "r") as file:
+        file.readline()  # Skip header
+        for line in file:
+            parts = line.strip().split('\t')
+            if len(parts) == 5:
+                name, start_date_str, priority, cost_estimate, completion = parts
+                start_date = datetime.datetime.strptime(start_date_str, "%d/%m/%Y").date()
+                project = Project(name, start_date, int(priority), float(cost_estimate), int(completion))
+                projects.append(project)
+    return projects
+
 
 main()
