8b38194e539282c9304f9566adad2187ee0decec
29034 <junjie.chu@my.jcu.edu.au>
2025-07-12 20:16:03 +0800

    Refactor return of is_complete and to_save_format function

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,13 +8,13 @@ import datetime
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion):
         self.name = name
-        self.start_date = start_date  # datetime.date
-        self.priority = priority      # int
-        self.cost_estimate = cost_estimate  # float
-        self.completion = completion  # int (percentage)
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion = completion
 
     def __str__(self):
-        return (f"{self.name}, start: {self.start_date('%d/%m/%Y')}, priority {self.priority}, "
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
                 f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion}%")
 
     def __lt__(self, other):
@@ -24,5 +24,5 @@ class Project:
         return self.completion >= 100
 
     def to_save_format(self):
-        return (f"{self.name}\t{self.start_date('%d/%m/%Y')}\t{self.priority}"
+        return (f"{self.name}\t{self.start_date}\t{self.priority}"
                 f"\t{self.cost_estimate}\t{self.completion}")
