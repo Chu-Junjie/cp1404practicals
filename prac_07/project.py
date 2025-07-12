176dafeea61307ea8184a8b56d74a79118a9e5bb
29034 <junjie.chu@my.jcu.edu.au>
2025-07-12 20:13:07 +0800

    Add is_complete and to_save_format function

--- prac_07/project.py
+++ prac_07/project.py
@@ -14,8 +14,15 @@ class Project:
         self.completion = completion  # int (percentage)
 
     def __str__(self):
-        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, "
+        return (f"{self.name}, start: {self.start_date('%d/%m/%Y')}, priority {self.priority}, "
                 f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion}%")
 
     def __lt__(self, other):
         return self.priority < other.priority
+
+    def is_complete(self):
+        return self.completion >= 100
+
+    def to_save_format(self):
+        return (f"{self.name}\t{self.start_date('%d/%m/%Y')}\t{self.priority}"
+                f"\t{self.cost_estimate}\t{self.completion}")
