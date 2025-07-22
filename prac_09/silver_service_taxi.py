af26d329f59a5794c1a7a456a3542b6e6336da69
29034 <junjie.chu@my.jcu.edu.au>
2025-07-22 16:42:36 +0800

    Create and add __init__ method

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+
+        self.price_per_km = Taxi.price_per_km * fanciness
