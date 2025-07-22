42d6a07de9a037c95b47016277c6d9f22601801b
29034 <junjie.chu@my.jcu.edu.au>
2025-07-22 16:20:37 +0800

    Create drive method

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,6 +4,7 @@ Car class
 """
 
 from prac_09.car import Car
+import random
 
 class unreliable_car(Car):
     """An unreliable car that sometimes doesn't drive."""
@@ -13,3 +14,9 @@ class unreliable_car(Car):
         super().__init__(name, fuel)
         self.reliability = reliability
 
+    def drive(self, distance):
+        """Drive the car only if a random number is less than reliability."""
+        if random.uniform(0,100) < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0
