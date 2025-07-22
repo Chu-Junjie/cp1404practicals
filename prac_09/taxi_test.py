1b7c3f1ce34bc4b2106f711966a1ea84b7eda376
29034 <junjie.chu@my.jcu.edu.au>
2025-07-22 15:44:51 +0800

    Add .start_fare() and print the details

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -5,5 +5,9 @@ def main():
     my_taxi.drive(40)
     print(my_taxi)
     print(f"Current fare: ${my_taxi.get_fare():.2f}")
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+    print(f"Current fare after restart: ${my_taxi.get_fare():.2f}")
 
 main()
