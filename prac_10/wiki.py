58df1be7ca2665d68cba002aa14e18dd825225eb
29034 <junjie.chu@my.jcu.edu.au>
2025-08-02 22:54:29 +0800

    Refactor as function format

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,19 +1,30 @@
 import wikipedia
 
-title = input("Enter page title: ").strip()
+def search_wikipedia():
+    print("Wikipedia Search Tool (press Enter to quit)\n")
+    for _ in range(10):
+        title = input("Enter page title: ").strip()
 
-try:
-    page = wikipedia.page(title, autosuggest=False)
-    print(page.title)
-    print(page.summary)
-    print(page.url)
+        if title != "":
+            try:
+                page = wikipedia.page(title, auto_suggest=False)
+                print(page.title)
+                print(page.summary)
+                print(page.url + "\n")
 
-except wikipedia.DisambiguationError as e:
-    print("We need a more specific title. Try one of the following:")
-    print(e.options)
+            except wikipedia.DisambiguationError as e:
+                print("We need a more specific title. Try one of the following:")
+                print("(BeautifulSoup warning)")
+                print(e.options[:5], "...")
 
-except wikipedia.PageError:
-    print(f'Page "{title}" does not match any pages. Try another title!')
+            except wikipedia.PageError:
+                print(f'Page "{title}" does not match any pages. Try another title!')
 
-except Exception as e:
-    print(f"An error occurred: {e}")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}\n")
+        else:
+            print("Thank you.")
+            return
+
+if __name__ == "__main__":
+    search_wikipedia()
